3b448dc2eb287f2e3f9e529b315db797f7232f4c
Stefan Matting <stefan@wire.com>
2022-11-09 17:48:06 +0100

    simplify custom.el

--- home/.doom.d/custom.el
+++ home/.doom.d/custom.el
@@ -3,47 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#191919" "#FF5E5E" "#468800" "#E9FDAC" "#8CDAFF" "#C586C0" "#85DDFF" "#d4d4d4"])
- '(custom-safe-themes
-   '("d47f868fd34613bd1fc11721fe055f26fd163426a299d45ce69bef1f109e1e71" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" "aaa4c36ce00e572784d424554dcc9641c82d1155370770e231e10c649b59a074" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "4a8d4375d90a7051115db94ed40e9abb2c0766e80e228ecad60e06b3b397acab" "99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" "188fed85e53a774ae62e09ec95d58bb8f54932b3fd77223101d036e3564f9206" "4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" "01cf34eca93938925143f402c2e6141f03abb341f27d1c2dba3d50af9357ce70" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "74ba9ed7161a26bfe04580279b8cad163c00b802f54c574bfa5d924b99daa4b9" "6c9cbcdfd0e373dc30197c5059f79c25c07035ff5d0cc42aa045614d3919dab4" "c4bdbbd52c8e07112d1bfd00fee22bf0f25e727e95623ecb20c4fa098b74c1bd" "f2927d7d87e8207fa9a0a003c0f222d45c948845de162c885bf6ad2a255babfd" "79278310dd6cacf2d2f491063c4ab8b129fee2a498e4c25912ddaa6c3c5b621e" "08a27c4cde8fcbb2869d71fdc9fa47ab7e4d31c27d40d59bf05729c4640ce834" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" default))
- '(exwm-floating-border-color "#121212")
- '(fci-rule-color "#515151")
- '(helm-completion-style 'emacs)
- '(highlight-tail-colors ((("#1d2416" "#1d2416") . 0) (("#232c30" "#202c30") . 20)))
- '(jdee-db-active-breakpoint-face-colors (cons "#171F24" "#FFFFFF"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#171F24" "#468800"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#171F24" "#777778"))
- '(objed-cursor-color "#FF5E5E")
- '(ormolu-extra-args
-   '("--ghc-opt" "-XAllowAmbiguousTypes" "--ghc-opt" "-XBangPatterns" "--ghc-opt" "-XConstraintKinds" "--ghc-opt" "-XDataKinds" "--ghc-opt" "-XDefaultSignatures" "--ghc-opt" "-XDerivingStrategies" "--ghc-opt" "-XDeriveFunctor" "--ghc-opt" "-XDeriveGeneric" "--ghc-opt" "-XDeriveLift" "--ghc-opt" "-XDeriveTraversable" "--ghc-opt" "-XEmptyCase" "--ghc-opt" "-XFlexibleContexts" "--ghc-opt" "-XFlexibleInstances" "--ghc-opt" "-XFunctionalDependencies" "--ghc-opt" "-XGADTs" "--ghc-opt" "-XInstanceSigs" "--ghc-opt" "-XKindSignatures" "--ghc-opt" "-XLambdaCase" "--ghc-opt" "-XMultiParamTypeClasses" "--ghc-opt" "-XMultiWayIf" "--ghc-opt" "-XNamedFieldPuns" "--ghc-opt" "-XNoImplicitPrelude" "--ghc-opt" "-XOverloadedStrings" "--ghc-opt" "-XPackageImports" "--ghc-opt" "-XPatternSynonyms" "--ghc-opt" "-XPolyKinds" "--ghc-opt" "-XQuasiQuotes" "--ghc-opt" "-XRankNTypes" "--ghc-opt" "-XScopedTypeVariables" "--ghc-opt" "-XStandaloneDeriving" "--ghc-opt" "-XTemplateHaskell" "--ghc-opt" "-XTupleSections" "--ghc-opt" "-XTypeApplications" "--ghc-opt" "-XTypeFamilies" "--ghc-opt" "-XTypeFamilyDependencies" "--ghc-opt" "-XTypeOperators" "--ghc-opt" "-XUndecidableInstances" "--ghc-opt" "-XViewPatterns"))
- '(pdf-view-midnight-colors (cons "#d4d4d4" "#191919"))
- '(rustic-ansi-faces
-   ["#191919" "#FF5E5E" "#468800" "#E9FDAC" "#8CDAFF" "#C586C0" "#85DDFF" "#d4d4d4"])
- '(safe-local-variable-values '((haskell-completion-backend . lsp)))
- '(vc-annotate-background "#191919")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#468800")
-    (cons 40 "#7caf39")
-    (cons 60 "#b2d672")
-    (cons 80 "#E9FDAC")
-    (cons 100 "#efd98e")
-    (cons 120 "#f5b671")
-    (cons 140 "#FC9354")
-    (cons 160 "#e98e78")
-    (cons 180 "#d78a9c")
-    (cons 200 "#C586C0")
-    (cons 220 "#d8789f")
-    (cons 240 "#eb6b7e")
-    (cons 260 "#FF5E5E")
-    (cons 280 "#dd6464")
-    (cons 300 "#bb6a6b")
-    (cons 320 "#997071")
-    (cons 340 "#515151")
-    (cons 360 "#515151")))
- '(vc-annotate-very-old-color nil)
- '(warning-suppress-log-types '((flycheck-after-syntax-check-hook))))
+ '(warning-suppress-types '((doom-first-buffer-hook) (initialization) (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
